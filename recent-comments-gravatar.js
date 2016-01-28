56034c3e4d8d335abef7ddcd1c8e15c47683380c
ticmaroc <oumouy@yahoo.fr>
2016-01-28 15:07:34 +0000

    Update recent-comments-gravatar.js

--- recent-comments-gravatar.js
+++ recent-comments-gravatar.js
@@ -1,1 +1,69 @@
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('9 G=G||5,8=8||1z,w=w||1w,m=m||"j://V.L.g/1i/?d=1b",F=F||" 1c &1D;",x=(A x===\'B\')?h:x,o=(A o===\'B\')?Q:o,q=(A q===\'B\')?h:q,r=(A r===\'B\')?Q:r;18 14(C){9 7;7=\'<R J="14">\';U(9 i=0;i<G;i++){9 z,y,2,k;4(i==C.O.e.D)X;7+="<10>";9 e=C.O.e[i];U(9 l=0;l<e.E.D;l++){4(e.E[l].19==\'1a\'){z=e.E[l].v;X}}y=e.M[0].1x.$t;2=e.M[0].1E$16.N;4(2.f("/P/")!=-1){2=2.p("/P/","/s"+8+"-c/")}b 4(2.f("/S/")!=-1){2=2.p("/S/","/s"+8+"-c/")}b 4(2.f("/T-c/")!=-1&&2.f("j:")!==0){2="j:"+2.p("/T-c/","/s"+8+"-c/")}b 4(2.f("H.g/n/17-W.I")!=-1){2="j://3.Y.Z.g/-1d-1e/1f/1U/1g/s"+8+"/1h.11"}b 4(2.f("H.g/n/1j-W.I")!=-1){2="j://3.Y.Z.g/-1k/1l/1m/1n/s"+8+"/1o.11"}b 4(2.f("H.g/n/1p.I")!=-1){4(m.f("L.g")!=-1){2=m+"&s="+8}b{2=m}}b{2=2}4(x===h){4(q===h){k="1q"}b{k=""}7+="<12 J=\\"1s "+k+"\\"><n J=\\""+k+"\\" N=\\""+2+"\\" 1t=\\""+y+"\\" 1u=\\""+8+"\\" 1v=\\""+8+"\\"/></12>"}7+="<a v=\\""+z+"\\">"+y+"</a>";9 13=e.1y.$t;9 6=13.p(/(<([^>]+)>)/1A,"");4(6!==""&&6.D>w){6=6.1B(0,w);6+="&1C;";4(o===h){6+="<a v=\\""+z+"\\">"+F+"</a>"}}b{6=6}7+="<u>"+6+"</u>";7+="</10>"}7+=\'</R>\';9 K="";4(r===h){K="15:1F;"}7+="<u 1G=\\"1H-1I:1J;15:1K;1L-1M:1N;"+K+"\\">1O 1P <a v=\\"j://V.1Q.1R/\\">1S</a></u>";1T.1r(7)}',62,119,'||authorAvatar||if||commBody|commentsHtml|avatarSize|var||else|||entry|indexOf|com|true||http|avatarClass||defaultAvatar|img|showMorelink|replace|roundAvatar|hideCredits|||span|href|characters|showAvatar|authorName|commentlink|typeof|undefined|w2b|length|link|moreLinktext|numComments|blogblog|gif|class|hideCSS|gravatar|author|src|feed|s1600|false|ul|s220|s512|for|www|rounded|break|bp|blogspot|li|png|div|commHTML|w2b_recent_comments|display|image|b16|function|rel|alternate|mm|More|AaI8|1X32ZM|TxMKLVzQ5BI|QYau8ov2blE|w2b_blogger_logo|avatar|openid16|9lSeVyNRKx0|TxMKMIqMNuI|AAAAAAAABYc|8iasY0xpLzc|w2b_openid_logo|blank|avatarRound|write|avatarImage|alt|width|height|40|name|content|60|ig|substring|hellip|raquo|gd|none|style|font|size|10px|block|text|align|right|Widget|by|way2blogging|org|Way2Blogging|document|AAAAAAAABYY'.split('|'),0,{}))
+var numComments = numComments || 5,
+avatarSize = avatarSize || 60,
+characters = characters || 40,
+defaultAvatar = defaultAvatar || "http://www.gravatar.com/avatar/?d=mm",
+moreLinktext = moreLinktext || " More &raquo;",
+showAvatar = (typeof showAvatar === 'undefined') ? true: showAvatar,
+showMorelink = (typeof showMorelink === 'undefined') ? false: showMorelink,
+roundAvatar = (typeof roundAvatar === 'undefined') ? true: roundAvatar,
+hideCredits = (typeof hideCredits === 'undefined') ? false: hideCredits;
+function w2b_recent_comments(w2b) {
+	var commentsHtml;
+	commentsHtml = '<ul class="w2b_recent_comments">';
+	for (var i = 0; i < numComments; i++) {
+		var commentlink, authorName, authorAvatar, avatarClass;
+		if (i == w2b.feed.entry.length) break;
+		commentsHtml += "<li>";
+		var entry = w2b.feed.entry[i];
+		for (var l = 0; l < entry.link.length; l++) {
+			if (entry.link[l].rel == 'alternate') {
+				commentlink = entry.link[l].href;
+				break
+			}
+		}
+		authorName = entry.author[0].name.$t;
+		authorAvatar = entry.author[0].gd$image.src;
+		if (authorAvatar.indexOf("/s1600/") != -1) {
+			authorAvatar = authorAvatar.replace("/s1600/", "/s" + avatarSize + "-c/")
+		} else if (authorAvatar.indexOf("/s220/") != -1) {
+			authorAvatar = authorAvatar.replace("/s220/", "/s" + avatarSize + "-c/")
+		} else if (authorAvatar.indexOf("/s512-c/") != -1 && authorAvatar.indexOf("http:") !== 0) {
+			authorAvatar = "http:" + authorAvatar.replace("/s512-c/", "/s" + avatarSize + "-c/")
+		} else if (authorAvatar.indexOf("blogblog.com/img/b16-rounded.gif") != -1) {
+			authorAvatar = "http://3.bp.blogspot.com/-AaI8-1X32ZM/TxMKLVzQ5BI/AAAAAAAABYY/QYau8ov2blE/s" + avatarSize + "/w2b_blogger_logo.png"
+		} else if (authorAvatar.indexOf("blogblog.com/img/openid16-rounded.gif") != -1) {
+			authorAvatar = "http://3.bp.blogspot.com/-9lSeVyNRKx0/TxMKMIqMNuI/AAAAAAAABYc/8iasY0xpLzc/s" + avatarSize + "/w2b_openid_logo.png"
+		} else if (authorAvatar.indexOf("blogblog.com/img/blank.gif") != -1) {
+			if (defaultAvatar.indexOf("gravatar.com") != -1) {
+				authorAvatar = defaultAvatar + "&s=" + avatarSize
+			} else {
+				authorAvatar = defaultAvatar
+			}
+		} else {
+			authorAvatar = authorAvatar
+		}
+		if (showAvatar === true) {
+			if (roundAvatar === true) {
+				avatarClass = "avatarRound"
+			} else {
+				avatarClass = ""
+			}
+			commentsHtml += "<div class=\"avatarImage " + avatarClass + "\"><img class=\"" + avatarClass + "\" src=\"" + authorAvatar + "\" alt=\"" + authorName + "\" width=\"" + avatarSize + "\" height=\"" + avatarSize + "\"/></div>"
+		}
+		commentsHtml += "<a href=\"" + commentlink + "\">" + authorName + "</a>";
+		var commHTML = entry.content.$t;
+		var commBody = commHTML.replace(/(<([^>]+)>)/ig, "");
+		if (commBody !== "" && commBody.length > characters) {
+			commBody = commBody.substring(0, characters);
+			commBody += "&hellip;";
+			if (showMorelink === true) {
+				commBody += "<a href=\"" + commentlink + "\">" + moreLinktext + "</a>"
+			}
+		} else {
+			commBody = commBody
+		}
+		commentsHtml += "<span>" + commBody + "</span>";
+		commentsHtml += "</li>"
+	}
+	document.write(commentsHtml)
+}
