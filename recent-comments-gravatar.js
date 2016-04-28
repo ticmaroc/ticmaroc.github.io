8444b37de2bf289e097fe7d2c735ba7d867345e3
ticmaroc <oumouy@yahoo.fr>
2016-04-28 18:05:16 +0100

    Update recent-comments-gravatar.js

--- recent-comments-gravatar.js
+++ recent-comments-gravatar.js
@@ -2,9 +2,7 @@ var numComments = numComments || 5,
     avatarSize = avatarSize || 60,
     characters = characters || 40,
     defaultAvatar = defaultAvatar || "http://www.gravatar.com/avatar/?d=mm",
-    moreLinktext = moreLinktext || " More »",
     showAvatar = typeof showAvatar === "undefined" ? true : showAvatar,
-    showMorelink = typeof showMorelink === "undefined" ? false : showMorelink,
     roundAvatar = typeof roundAvatar === "undefined" ? true : roundAvatar;
 
 function w2b_recent_comments(h) {
@@ -36,9 +34,7 @@ function w2b_recent_comments(h) {
         b = b.content.$t.replace(/(<([^>]+)>)/ig, "");
         if (b !== "" && b.length > characters) {
             b = b.substring(0, characters);
-            b += "…";
-            if (showMorelink === true) b += '<a href="' + f + '">' + moreLinktext + "</a>"
-        }
+            b += "…" }
         d += "<span>" + b + "</span>";
         d += "</li>"
     }
