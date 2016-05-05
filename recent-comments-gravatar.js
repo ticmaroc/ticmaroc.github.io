61770109ab1533d02ae08de9cee7b8c1296389c4
ticmaroc <oumouy@yahoo.fr>
2016-05-05 23:03:37 +0100

    Update recent-comments-gravatar.js

--- recent-comments-gravatar.js
+++ recent-comments-gravatar.js
@@ -20,8 +20,8 @@ function w2b_recent_comments(h) {
         c = b.author[0].name.$t;
         a = b.author[0].gd$image.src;
         if (a.indexOf("/s512-c/") != -1) a = a.replace("/s512-c/", "/s" + avatarSize + "-c/");
-		else if (a.indexOf("blogblog.com/img/b16-rounded.gif") != -1) a = "http://3.bp.blogspot.com/-AaI8-1X32ZM/TxMKLVzQ5BI/AAAAAAAABYY/QYau8ov2blE/s" + avatarSize + "/w2b_blogger_logo.png";
-         else if (a.indexOf("blogblog.com/img/blank.gif") != -1) a = "http://3.bp.blogspot.com/-AaI8-1X32ZM/TxMKLVzQ5BI/AAAAAAAABYY/QYau8ov2blE/s" + avatarSize + "/w2b_blogger_logo.png";
+		else if (a.indexOf("blogblog.com/img/b16-rounded.gif") != -1) a = "https://3.bp.blogspot.com/-C312WZhuNUs/VyvCxuadqnI/AAAAAAAAH8A/yPxV1LBOSdsGFp-iqQpqKEAoGJkKN28NQCLcB/s" + avatarSize + "/comment.png";
+         else if (a.indexOf("blogblog.com/img/blank.gif") != -1) a = "https://3.bp.blogspot.com/-C312WZhuNUs/VyvCxuadqnI/AAAAAAAAH8A/yPxV1LBOSdsGFp-iqQpqKEAoGJkKN28NQCLcB/s" + avatarSize + "/comment.png";
        if (showAvatar === true) {
             g = roundAvatar === true ? "avatarRound" : "";
             d += '<div class="avatarImage ' + g + '"><img class="' + g + '" src="' + a + '" alt="' + c + '" width="' + avatarSize + '" height="' + avatarSize + '"/></div>'
